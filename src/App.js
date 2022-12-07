b41e2b39266db8097e7d2c885fbc675ee9007a58
ersan80 <ersan.kuleli80@gmail.com>
2022-12-07 20:42:43 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import "./scss/App.scss";
+import { toastsuccess, toastwarn } from "./toastify/Toastify";
+
+function App() {
+  const [task, setTask] = useState([]);
+  const url = " http://localhost:3002/task";
+
+  const getData =  () => {
+    fetch(url).then((res)=>res.json()).then((data)=>setTask(data));
+/*     const data = response.json();
+    setTask(data); */
+  };
+
+  const postData =  (data) => {
+     axios.post(url, data);
+    getData();
+    toastsuccess("New Cargo Added Successfully");
+  };
+  const deleteData =  (id) => {
+   axios.delete(url + `/${id}`);
+    getData();
+    toastwarn("Cargo information deleted successfuly");
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <Home task={task} postData={postData} deleteData={deleteData} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import PhoneIcon from "@mui/icons-material/Phone";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import DescriptionIcon from "@mui/icons-material/Description";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import "../scss/Table.scss";
+
+const Table = ({ task, search, deleteData }) => {
+  const charCode = search.charCodeAt();
+  const [notFound, setNotFound] = useState(
+    "The cargo information you were looking for was not found"
+  );
+  const [filter, setFilter] = useState([]);
+  const filtered = () => {
+    const data = task?.filter(
+      (item) =>
+        item?.name?.toLowerCase().includes(search) ||
+        item?.policyNo?.toLowerCase().includes(search) ||
+        item?.phone?.toLowerCase().includes(search) ||
+        item?.email?.toLowerCase().includes(search)
+    );
+    setFilter(data);
+  };
+
+  useEffect(() => {
+    filtered();
+  }, [search]);
+
+
+  return (
+    <div className="tableContainer">
+      {!(charCode === 32) && search && (
+        <div className="div">
+          {filter.length > 0 ? (
+            filter?.map((item, index) => {
+              const { id, name, email, phone, policyNo } = item;
+              return (
+                <div key={id}>
+                  {filter && (
+                    <div className="container">
+                      <div className="nameDiv">{name}</div>
+
+                      <div className="infoBox">
+                        <div>
+                          <p>
+                            <PhoneIcon /> {phone}
+                          </p>
+                        </div>
+                        <div>
+                          <p>
+                            <AlternateEmailIcon /> {email}
+                          </p>
+                        </div>
+                        <div>
+                          <p>
+                            <DescriptionIcon /> {policyNo}
+                          </p>
+                        </div>
+
+                        <div>
+                          <p
+                            className="deleteCell"
+                            onClick={() => deleteData(id)}
+                          >
+                            <DeleteForeverIcon className="deleteIcon" />
+                            Delete
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              );
+            })
+          ) : (
+            <p style={{ backgroundColor: "red" }}>{notFound}</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Table;
